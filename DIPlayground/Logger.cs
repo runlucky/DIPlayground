22431c2233283b9937bf5a603024da4dff0988a5
Kakeru Fukuda <stand.by.we@gmail.com>
2019-10-01 00:20:59 +0900

    ねむい

---
+++ DIPlayground/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace DIPlayground
+{
+    interface ILogger
+    {
+        void Log(string text);
+    }
+
+    class Logger : ILogger
+    {
+        public void Log(string text) => Debug.WriteLine(text);
+    }
+
+    class DammyLogger : ILogger
+    {
+        public void Log(string text)
+        {
+            // nop
+        }
+    }
+}
---
+++ DIPlayground/Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroResolver;
+
+namespace DIPlayground
+{
+    interface IUser
+    {
+        public string Name { get; }
+    }
+
+    class ConstructorUser : IUser
+    {
+        private ILogger logger;
+
+        [Inject]
+        public ConstructorUser(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public string Name => throw new NotImplementedException();
+    }
+
+    class PropertyUser : IUser
+    {
+
+    }
+
+    class MethodUser:IUser
+    {
+
+    }
+
+    class FieldUser: IUser
+    {
+
+    }
+}
